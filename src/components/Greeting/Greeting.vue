c0871210dd3aaa1a87ad1bd3f3a1ab8ef8e60ba9
Victoria Borodina <Borodina.V.Serg@sberbank.ru>
2018-11-26 11:13:47 +0300

    adds webpack.config

---
+++ src/components/Greeting/Greeting.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>{{ greeting }}</h1>
+</template>
+
+<script>
+    module.exports = {
+        data: {
+            greeting: 'Привет Webpack',
+        },
+    };
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const { VueLoaderPlugin } = require('vue-loader');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/app.js'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundel.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /.js$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    // devtool: 'sourse-map cheap-module-source-map',
+    plugins: [
+        // HotModuleReplacementPlugin
+        // CleanWebpackPlugin
+        // UnusedWebpackPlugin //'README.md',
+        new VueLoaderPlugin(),
+        new CopyWebpackPlugin([
+            { from: path.resolve(__dirname, './src/index.html') }
+        ], {}),
+    ]
+};
